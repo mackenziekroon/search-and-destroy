199f03910e134554ec51c78c25b052b29459068e
Mackenzie Kroon <kroonmackenzie@gmail.com>
2020-10-28 14:58:50 -0500

    bst

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -1,8 +1,43 @@
-'use strict';
+"use strict";
+
+// edge case - if array is empty return false
+
+//1. if/else checking is the array is empty, if so, return false
+//2. pick a midpoint in the array and split the array at that point
+//3. search the first array for the target
+//4. if we find it - return true
+//5. if we don't find it - then we search the second array
+// repeat step 4
+//6. if we don't find it - return false
+
+// [1,3,4,5,10,15,20,22,23,98,1000];
 
 // Complete this algo
 const binarySearch = (array, target) => {
-	
+  // console.log("ARRAY->", array);
+  if (array.length === 0) {
+    return false;
+  }
+  let midpoint = Math.floor(array.length / 2);
+  // console.log("midpoint index--->", midpoint);
+  // console.log("the midpoint of the array", array[midpoint]);
+  if (target < array[midpoint]) {
+    for (let i = 0; i < array[midpoint]; i++) {
+      // console.log("1. i-->", i);
+      if (array[i] === target) {
+        // console.log("1. array[i]-->", array[i]);
+        return true;
+      }
+    }
+  } else if (target >= array[midpoint]) {
+    for (let i = midpoint; i < array.length; i++) {
+      // console.log("2. i-->", i);
+      if (array[i] === target) {
+        return true;
+      }
+    }
+  }
+  return false;
 };
 
 /*
@@ -14,4 +49,4 @@ const binarySearch = (array, target) => {
 
 */
 
-module.exports = binarySearch
+module.exports = binarySearch;
